fa9a78d3de4d0509b2b29e4522d761ba8d120e7d
Ana Elisa <elisaibz92@gmail.com>
2025-01-07 12:02:34 +0100

    btn-reset

--- src/js/btn_reset.js
+++ src/js/btn_reset.js
@@ -2,7 +2,7 @@
 
 const resetBtn = document.querySelector('.js-btnreset');
 const myForm = document.querySelector('.js-form');
-const myCard = document.querySelector('.js-sectionCard');// no se como resetear la tarjeta
+const myCard = document.querySelector('.js-sectionCard');//no se como resetear la tarjeta
 
 //Añadimos evento al boton
 resetBtn.addEventListener('click', () =>{

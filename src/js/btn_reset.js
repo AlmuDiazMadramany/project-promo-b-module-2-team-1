03aa02dc6d3afd7a8ea0b40eb755bc46249c4abc
Ana Elisa <elisaibz92@gmail.com>
2025-01-07 13:32:50 +0100

    fixed issues reset btn

--- src/js/btn_reset.js
+++ src/js/btn_reset.js
@@ -4,11 +4,7 @@ const resetBtn = document.querySelector('.js-btnreset');
 const myForm = document.querySelector('.js-form');
 const myCard = document.querySelector('.js-sectionCard');//no se como resetear la tarjeta
 
-//Añadimos evento al boton
 resetBtn.addEventListener('click', () =>{
-    //restablecemos el form
-    myForm.reset();
-    myCard.reset();
-    console.log("formulario reseteado");
+    location.reload();
 
 });

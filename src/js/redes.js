2b90c4519fb932d32f87a50b744fc8201b35ba81
Ana Maria <naoseidenada0418@gmail.com>
2025-01-07 09:05:52 +0100

    corrections JS

--- src/js/redes.js
+++ src/js/redes.js
@@ -1,7 +1,7 @@
 //File js usado para añadir evento en el botón de "generar" y un efecto hidde en los iconos.
 
 const btnRedes = document.querySelector('.js-btn');
-const iconRedes = document.querySelector('js-icon');
+const iconRedes = document.querySelector('.js-icon');
 
 btnRedes.addEventListener('click', () => {iconRedes.classList.toggle('hidden')
 });

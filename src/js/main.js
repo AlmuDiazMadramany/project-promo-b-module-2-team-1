9c3f06e5464f514d24056d90ec826d6001c01dca
AlmuDiazMadramany <almumadra@hotmail.com>
2025-01-07 20:55:25 +0100

    preview ok

--- src/js/main.js
+++ src/js/main.js
@@ -31,14 +31,24 @@ const ingredientes = document.querySelector('.recipe2_ingredients_input');
 const pasos = document.querySelector('.recipe2_steps_input');
 // const imagen = document.querySelector('.recipe1_image');
 
+// Textos de ejemplo
+const placeholders = {
+    field1: '3 personas',
+    field3: 'Título de la receta',
+    field4: '50 minutos',
+    field5: 'Ejemplo: 2 pechugas de pollo. 1 pimiento rojo. 1 calabacín.1 zanahoria. 1 diente de ajo. 2 cucharadas de aceite de oliva. Sal y pimienta al gusto.',
+    field6: 'Ejemplo: 1. Lavar y cortar las verduras en tiras finas Picar el ajo finamente. Sazona las pechugas de pollo con sal y pimienta. 2. En una sartén grande, calentar 1 cucharada de aceite de oliva a fuego medio. Añade las verduras y el ajo, y saltea durante 5-7 minutos hasta que estén tiernas pero crujientes. Retira y reserva. 3. En la misma sartén, añade la otra cucharada de aceite y cocina las pechugas de pollo a fuego medio-alto durante 4-5 minutos por cada lado, o hasta que estén doradas y bien cocidas por dentro. 4. Devuelve las verduras a la sartén con el pollo para calentarlas ligeramente y mezclar los sabores. 5. Sirve y disfruta. Coloca el pollo en un plato, acompáñalo con las verduras y sirve caliente.',
+};
+
+
 
 // Definimos la función de renderizado
 function render() {
-    comensales.innerHTML = data.field1;
-    nombre.innerHTML = data.field3;
-    tiempo.innerHTML = data.field4;
-    ingredientes.innerHTML = data.field5;
-    pasos.innerHTML = data.field6;
+    comensales.innerHTML = data.field1 || placeholders.field1;
+    nombre.innerHTML = data.field3 || placeholders.field3;
+    tiempo.innerHTML = data.field4 || placeholders.field4;
+    ingredientes.innerHTML = data.field5 || placeholders.field5;
+    pasos.innerHTML = data.field6 || placeholders.field6;
 }
 
 // Definimos la función manejadora de eventos
@@ -53,3 +63,4 @@ function handleForm(event) {
 // Llamamos a la función que actualiza la preview
 form.addEventListener('input', handleForm);
 
+render ();
